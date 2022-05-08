dfde007c1dd2328e5fc3ba15798ea7ba6abe81a3
Jamie <iamjevawin+gh@gmail.com>
2022-05-08 18:49:12 +0100

    ⚡️ promise loop through screenshots

--- captures.mjs
+++ captures.mjs
@@ -1,13 +1,24 @@
 import captureWebsite from "capture-website";
 import { readFile } from "fs/promises";
-const sites = JSON.parse(
-  await readFile("src/includes/scripts/sites.json")
-);
-for (const [key, value] of Object.entries(sites)) {
-  value.games.forEach(async game => {
-    await captureWebsite.file(
-      game.url,
-      `dist/screenshots/${encodeURI(game.name).toLowerCase()}.png`
-    );
-  })
-}
+
+// capture settings
+const settings = {
+  emulateDevice: "iPad",
+  type: "webp",
+};
+
+// get urls
+const sites = JSON.parse(await readFile("src/includes/scripts/sites.json"));
+
+// loop and capture
+(async () => {
+  for await (const [key, value] of Object.entries(sites)) {
+    for await (const game of value.games) {
+      await captureWebsite.file(
+        game.url,
+        `dist/screenshots/${encodeURI(game.name).toLowerCase()}.png`,
+        settings
+      );
+    };
+  }
+})();
