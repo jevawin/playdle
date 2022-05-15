45757228578abe8d8253af2d4c13cc1721fed9d7
Jamie <iamjevawin+gh@gmail.com>
2022-05-15 10:28:58 +0100

    ♿️  image alt text

--- src/includes/scripts/input.js
+++ src/includes/scripts/input.js
@@ -17,7 +17,7 @@ fetch("sites.json")
               html += `
               <div class="rounded overflow-hidden shadow flex flex-col justify-start p-2 bg-slate-600 bg-[url('${image}')]">
                 <img src="${image}"
-                  class="w-full h-auto rounded overflow-hidden" loading="lazy" />
+                  class="w-full h-auto rounded overflow-hidden" loading="lazy" alt="${game.name}: ${game.description}" />
                 <h2 class="font-bold text-lg mt-2">${game.name}</h2>
                 <p class="mb-2">${game.description}</p>
                 <a target="_blank" href="${game.url}" class="block rounded bg-green-600 text-white font-bold px-2 py-3 text-center border-green-700 border-b-2 border-r-2 active:border-r-0 active:border-b-0 transition-all justify-self-end mt-auto">Play ${game.name}</a>
