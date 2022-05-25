62824caeb46b3946b548f2aa5c53250cd89146aa
Jamie <iamjevawin+gh@gmail.com>
2022-05-25 22:16:23 +0100

    ⚡️ improve stdout/stderr, remove webp, Stadiumdle

--- captures.mjs
+++ captures.mjs
@@ -3,8 +3,8 @@ import { readFile } from "fs/promises";
 
 // capture settings
 const settings = {
-  width: 300,
-  height: 225,
+  width: 600,
+  height: 400,
   overwrite: true,
   type: "jpeg",
   removeElements: ["#pz-gdpr", "[id*='sp_message_container']"],
@@ -16,12 +16,16 @@ const sites = JSON.parse(await readFile("src/includes/scripts/sites.json")).site
 // loop and capture
 (async () => {
   for await (const [key, value] of Object.entries(sites)) {
-    for (const game of value.games) {
+    for await (const game of value.games) {
+      const start = new Date().getTime();
+      console.log(`Capturing ${game.name}`);
       await captureWebsite.file(
         game.url,
         `dist/screenshots/${encodeURI(game.name.replaceAll(" ", "_")).toLowerCase()}.${settings.type}`,
         settings
       );
+      const end = new Date().getTime();
+      console.log(`Captured ${game.name} in ${end - start}ms`);
     }
   }
 })();
